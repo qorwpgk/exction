58ec5661bdb9bc3ef24f1adc83ff600c896f1185
qorwpgk <48539270+qorwpgk@users.noreply.github.com>
2020-07-21 11:41:01 +0900

    Update hello.java
    Java hello!!

--- testjava/src/testjava/hello.java
+++ testjava/src/testjava/hello.java
@@ -3,7 +3,7 @@ package testjava;
 public class hello {
 	public static void main(String[] args) {
 
-	     System.out.println("hello world!");
+	     System.out.println("Java Hello!!");
 	     
 	  }
 }
